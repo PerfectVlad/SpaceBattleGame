ccc44f1372a852a6d4726389604935a3b8d17cf1
PerfectVlad <vlad-log03@mail.ru>
2025-10-12 01:11:04 +0600

    Реализованы системы поворота объектов

--- SpaceBattle/Interfaces/IRotatable.cs
+++ SpaceBattle/Interfaces/IRotatable.cs
@@ -4,25 +4,10 @@ namespace SpaceBattle.Interfaces
 {
     public interface IRotatable
     {
-        /// <summary>
-        /// Текущий угол поворота объекта в градусах
-        /// </summary>
-        /// <returns>Угол в градусах (0-360)</returns>
-        /// <exception cref="UnableToGetAngleException">Если невозможно прочитать угол</exception>
         int GetAngle();
 
-        /// <summary>
-        /// Угловая скорость объекта в градусах
-        /// </summary>
-        /// <returns>Угловая скорость в градусах</returns>
-        /// <exception cref="UnableToGetAngularVelocityException">Если невозможно прочитать угловую скорость</exception>
         int GetAngularVelocity();
 
-        /// <summary>
-        /// Установить новый угол поворота объекта
-        /// </summary>
-        /// <param name="newAngle">Новый угол в градусах</param>
-        /// <exception cref="UnableToSetAngleException">Если невозможно установить угол</exception>
         void SetAngle(int newAngle);
     }
 }
